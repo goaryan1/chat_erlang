5a9938e5a057c52271dde236c10f9cef93f81cf2
Jugal-GreyOrange <158149871+Jugal-GreyOrange@users.noreply.github.com>
2024-02-09 15:20:45 +0530

    Update client.erl

--- client.erl
+++ client.erl
@@ -11,10 +11,8 @@ start() ->
 start_helper(ClientStatus) ->
     {ok, Socket} = gen_tcp:connect('localhost', 9991, [binary, {active, true}]),
     gen_tcp:recv(Socket, 0),
-    io:format("1. ~p~n", [Socket]),
     receive
         {tcp, Socket, BinaryData} ->
-            % io:format("2. ~p~n", [Socket]),
             Data = erlang:binary_to_term(BinaryData),
             case Data of
                 {connected, Name, MessageHistory} ->
@@ -23,16 +21,15 @@ start_helper(ClientStatus) ->
                     ClientStatus1 = ClientStatus#client_status{serverSocket = Socket, name = Name},
                     loop(ClientStatus1);
                 {reject, Message} ->
-                    io:format("~s~n",[Message])
+                    io:format("Error : ~s~n",[Message])
             end;
         {tcp_closed, Socket} ->
-            io:format("not connected to the server: ~n")
+            io:format("Not connected to the server: ~n")
     end.
 
 loop(ClientStatus) ->
     Socket = ClientStatus#client_status.serverSocket,
     StartPid = ClientStatus#client_status.startPid,
-    % io:format("StartPid = ~p~n", [StartPid]),
     gen_tcp:recv(Socket, 0),    % activate listening
     receive
         {tcp, Socket, BinaryData} ->
@@ -49,46 +46,35 @@ loop(ClientStatus) ->
             ok;
         {StartPid, {private_message, Message, Receiver}} ->
             BinaryData = term_to_binary({private_message, Message, Receiver}),
-            gen_tcp:send(Socket, BinaryData);
+            gen_tcp:send(Socket, BinaryData),
+            private_message_helper(ClientStatus);
         {StartPid, {message, Message}} ->
-            io:format("Message received from startPid~n"),
-            io:format("sending to socket: ~p~n", [Socket]),
             BinaryData = term_to_binary({message, Message}),
             gen_tcp:send(Socket, BinaryData);
         {StartPid, {exit}} ->
             BinaryData = term_to_binary({exit}),
-            gen_tcp:send(Socket, BinaryData);   % close the process and print exit message
+            gen_tcp:send(Socket, BinaryData); 
         {StartPid, {show_clients}} ->
             BinaryData = term_to_binary({show_clients}),
             gen_tcp:send(Socket, BinaryData),
             ClientList = get_client_list(ClientStatus),
-            io:format("~p~n", [ClientList]),
             print_list(ClientList)
-        % {StartPid, {set_name, NewName}} ->
-        %     BinaryData = term_to_binary({set_name, NewName}),
-        %     gen_tcp:send(Socket, BinaryData),
-        %     set_name_handler(ClientStatus, NewName)
     end,
     loop(ClientStatus).
 
-% set_name_handler(#client_status{} = ClientStatus, NewName) ->
-%     io:format("70"),
-%     Socket = ClientStatus#client_status.serverSocket,
-%     gen_tcp:recv(Socket, 0),
-%     receive
-%         {tcp, Socket, BinaryData} ->
-%             Data = binary_to_term(BinaryData),
-%             io:format("Data received: ~p~n", Data),
-%             case Data of
-%                 {error, Message} ->
-%                     io:format("error while updating username: ~p~n", [Message]),
-%                     ClientStatus;
-%                 {success, _Message} ->
-%                     io:format("username updated to ~p~n", [NewName]),
-%                     ClientStatus1 = ClientStatus#client_status{name = NewName},
-%                     ClientStatus1
-%             end
-%     end.
+private_message_helper(#client_status{} = ClientStatus) ->
+    Socket = ClientStatus#client_status.serverSocket,
+    gen_tcp:recv(Socket, 0),
+    receive
+        {tcp, Socket, BinaryData} ->
+            Data = binary_to_term(BinaryData),
+            case Data of
+                {success, _Message} ->
+                    ok;
+                {error, Message} ->
+                    io:format("Error : ~s~n",[Message])
+            end
+    end.
 
 get_client_list(#client_status{} = ClientStatus) ->
     Socket = ClientStatus#client_status.serverSocket,
@@ -132,8 +118,48 @@ print_list(List) ->
     lists:foreach(fun(X) ->
         io:format("~p~n", [X]) end, List).
 
+
+% ----------------------------------
+
+
+
+
+
+
+
+
+
+% ----------Unused functions-------------
+
 % set_name() ->
 %     NewName = string:trim(io:get_line("Enter desired username: ")),
 %     StartPid = get(startPid),
 %     SpawnedPid = get(spawnedPid),
 %     SpawnedPid ! {StartPid, {set_name, NewName}}.
+
+
+
+% set_name_handler(#client_status{} = ClientStatus, NewName) ->
+%     io:format("70"),
+%     Socket = ClientStatus#client_status.serverSocket,
+%     gen_tcp:recv(Socket, 0),
+%     receive
+%         {tcp, Socket, BinaryData} ->
+%             Data = binary_to_term(BinaryData),
+%             io:format("Data received: ~p~n", Data),
+%             case Data of
+%                 {error, Message} ->
+%                     io:format("error while updating username: ~p~n", [Message]),
+%                     ClientStatus;
+%                 {success, _Message} ->
+%                     io:format("username updated to ~p~n", [NewName]),
+%                     ClientStatus1 = ClientStatus#client_status{name = NewName},
+%                     ClientStatus1
+%             end
+%     end.
+
+
+% {StartPid, {set_name, NewName}} ->
+%     BinaryData = term_to_binary({set_name, NewName}),
+%     gen_tcp:send(Socket, BinaryData),
+%     set_name_handler(ClientStatus, NewName)

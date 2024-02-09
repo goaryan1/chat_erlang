8a70ee3e58b01f2bcda69ef040b9c621496cd05d
Jugal-GreyOrange <158149871+Jugal-GreyOrange@users.noreply.github.com>
2024-02-09 15:20:30 +0530

    Update server.erl

--- server.erl
+++ server.erl
@@ -1,31 +1,37 @@
 -module(server).
--export([start/0, accept_clients/3, broadcast/1, broadcast/2, remove_client/1, show_clients/0, print_messages/1, loop/1]).
+-export([start/0, accept_clients/1, broadcast/1, broadcast/2, remove_client/1, show_clients/0, print_messages/1, loop/1]).
 -record(client, {clientSocket, clientName}).
--record(message, {timestamp, senderName, text}). %senderName corresponds to #client.clientName
+-record(message, {timestamp, senderName, text}).
+-record(server_status, {listenSocket, counter, maxClients, historySize}).
 -include_lib("stdlib/include/qlc.hrl").
 
 start() ->
     init_databases(),
     {N,[]} =  string:to_integer(string:trim(io:get_line("Enter No of Clients Allowed : "))),
-    {ok, ListenSocket} = gen_tcp:listen(9990, [binary, {packet, 0}, {active, true}]),
-    io:format("Server listening on port 9990 and Socket : ~p ~n",[ListenSocket]),
+    {X,[]} =  string:to_integer(string:trim(io:get_line("Message History Size : "))),
+    {ok, ListenSocket} = gen_tcp:listen(9991, [binary, {packet, 0}, {active, true}]),
+    io:format("Server listening on port 9991 and Socket : ~p ~n",[ListenSocket]),
     Counter = 1,
-    spawn(server, accept_clients, [ListenSocket, Counter, N]).
+    ServerStatus = #server_status{listenSocket = ListenSocket, counter = Counter, maxClients = N, historySize = X},
+    spawn(server, accept_clients, [ServerStatus]).
 
 init_databases() ->
     mnesia:start(),
     mnesia:create_table(client, [{attributes, record_info(fields, client)}]),
     mnesia:create_table(message, [{attributes, record_info(fields, message)}, {type, ordered_set}]).
 
-accept_clients(ListenSocket, Counter, N) ->
+accept_clients(ServerStatus) ->
+    ListenSocket = ServerStatus#server_status.listenSocket,
+    MaxClients = ServerStatus#server_status.maxClients,
+    HistorySize = ServerStatus#server_status.historySize,
+    Counter = ServerStatus#server_status.counter,
     {ok, ClientSocket} = gen_tcp:accept(ListenSocket),
     Active_clients = active_clients(),
-    io:format("~p~n",[Active_clients]),
     if 
-        Active_clients<N ->
+        Active_clients<MaxClients ->
             ClientName = "User" ++ integer_to_list(Counter),
             io:format("Accepted connection from ~p~n", [ClientName]),
-            MessageHistory = retreive_messages(10),
+            MessageHistory = retreive_messages(HistorySize),
             Data = {connected, ClientName, MessageHistory},
             BinaryData = erlang:term_to_binary(Data),
             gen_tcp:send(ClientSocket, BinaryData),
@@ -33,24 +39,22 @@ accept_clients(ListenSocket, Counter, N) ->
             Message = ClientName ++ " joined the ChatRoom.",
             broadcast({ClientSocket, Message}),
             NewCounter = Counter + 1,
+            ServerStatus1 = ServerStatus#server_status{counter = NewCounter},
             ListenPid = spawn(server, loop, [ClientSocket]),
             gen_tcp:controlling_process(ClientSocket, ListenPid),
-            accept_clients(ListenSocket, NewCounter, N);
+            accept_clients(ServerStatus1);
         true->
             Message = "No Space on Server :(",
             gen_tcp:send(ClientSocket, term_to_binary({reject,Message})),
             gen_tcp:close(ClientSocket),
-            accept_clients(ListenSocket, Counter, N)
+            accept_clients(ServerStatus)
     end.
-
-    
 
 loop(ClientSocket) ->
     gen_tcp:recv(ClientSocket, 0),
     receive
         {tcp, ClientSocket, BinaryData} ->
             Data = binary_to_term(BinaryData),
-            io:format("Message received: ~p~n", [Data]),
             case Data of
                 % Private Message
                 {private_message, Message, Receiver} ->
@@ -66,34 +70,19 @@ loop(ClientSocket) ->
                 {show_clients} ->
                     List = retreive_clients(),
                     gen_tcp:send(ClientSocket, term_to_binary({List})),
-                    io:format("~p~n",[List]),
                     loop(ClientSocket);
                 % Exit from ChatRoom
                 {exit} ->
                     io:format("Client ~p left the ChatRoom.~n",[getUserName(ClientSocket)]),
                     LeavingMessage = getUserName(ClientSocket) ++ " left the ChatRoom.",
                     broadcast({ClientSocket, LeavingMessage}),
-                    remove_client(ClientSocket);
-                {set_name, NewName} ->
-                    io:format("inside set_name~n"),
-                    case userNameUsed(NewName) of
-                        true ->
-                            io:format("name is used~n"),
-                            gen_tcp:send(ClientSocket, term_to_binary({error, "Name already in use"}));
-                        false ->
-                            io:format("name is unused~n"),
-                            updateName(ClientSocket, NewName),
-                            gen_tcp:send(ClientSocket, term_to_binary({success, "Name updated to " ++ NewName}))
-                end
+                    remove_client(ClientSocket)
             end;
         % Client Connection lost
         {tcp_closed, ClientSocket} ->
             io:format("Client ~p disconnected~n", [getUserName(ClientSocket)]),
             remove_client(ClientSocket)
     end.
-
-updateName(ClientSocket, NewName) ->
-    mnesia:write(#client{clientName =  NewName, clientSocket = ClientSocket}).
 
 insert_client_database(ClientSocket, ClientName) ->
     ClientRecord = #client{clientSocket=ClientSocket, clientName = ClientName},
@@ -102,19 +91,10 @@ insert_client_database(ClientSocket, ClientName) ->
     end).
 
 insert_message_database(ClientName, Message) ->
-
         MessageRecord = #message{timestamp = os:timestamp(), senderName = ClientName, text = Message},
         mnesia:transaction(fun() ->
             mnesia:write(MessageRecord)
         end).
-
-userNameUsed(UserName) ->
-    case mnesia:dirty_read({client, UserName}) of
-        [] ->
-            false;
-        [_] ->
-            true
-    end.
 
 active_clients() ->
     Trans = fun() -> mnesia:all_keys(client) end,
@@ -141,11 +121,16 @@ getSocket(Name) ->
 
 broadcast({SenderSocket, Message}, Receiver) ->
     % private messages don't get saved in the database
-    io:format("receiver : ~p~n", [Receiver]),
     RecSocket = getSocket(Receiver),
     SenderName = getUserName(SenderSocket),
-    io:format("RecScoket : ~p, Sendername : ~p~n",[RecSocket, SenderName]),
-    gen_tcp:send(RecSocket, term_to_binary({message, SenderName, Message})).
+    case RecSocket of
+        {error, not_found} ->
+            gen_tcp:send(SenderSocket, term_to_binary({error, "User not found"}));
+        RecvSocket ->
+            io:format("RecScoket : ~p, Sendername : ~p~n",[RecvSocket, SenderName]),
+            gen_tcp:send(RecvSocket, term_to_binary({message, SenderName, Message})),
+            gen_tcp:send(SenderSocket, {success, "Message Succesfully Sent"})
+    end.
 
 broadcast({SenderSocket, Message}) ->
     SenderName = getUserName(SenderSocket),
@@ -193,10 +178,52 @@ retreive_messages(N) ->
     {atomic, Query} = mnesia:transaction(F),
     ReverseMessages = lists:sublist(lists:reverse(Query), 1, N),
     Messages = lists:reverse(ReverseMessages),
-    Messages.
+    % io:format("~p~n",[Messages]),
+    % Messages.
+    MessageHistory = lists:map(fun(X) ->  
+                        {message,_,SenderName,Text} = X,
+                        MsgString = SenderName ++  " : " ++ Text,
+                        MsgString
+                    end,  Messages),
+    MessageHistory.
 
 print_messages(N) ->
     Messages = retreive_messages(N),
     io:format("Messages:~n"),
     lists:foreach(fun(X) ->
         io:format("~p~n", [X]) end, Messages).
+
+% -----------------------------------------
+
+
+
+
+
+
+
+% ----------Unused functions--------------
+
+
+% userNameUsed(UserName) ->
+%     case mnesia:dirty_read({client, UserName}) of
+%         [] ->
+%             false;
+%         [_] ->
+%             true
+%     end.
+
+
+% updateName(ClientSocket, NewName) ->
+%     mnesia:write(#client{clientName =  NewName, clientSocket = ClientSocket}).
+
+
+% {set_name, NewName} ->
+%     case userNameUsed(NewName) of
+%         true ->
+%             io:format("name is used~n"),
+%             gen_tcp:send(ClientSocket, term_to_binary({error, "Name already in use"}));
+%         false ->
+%             io:format("name is unused~n"),
+%             updateName(ClientSocket, NewName),
+%             gen_tcp:send(ClientSocket, term_to_binary({success, "Name updated to " ++ NewName}))
+%     end
